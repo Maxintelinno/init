c493a7dd6d78f660fa19e8e0fe0d08ffeff986e7
Osacn thakrit Ratchakote <osacn_thakrit.ratchakote@krungthai.com>
2026-02-19 23:30:00 +0700

    init 2

--- main.go
+++ main.go
@@ -2,30 +2,32 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	// Echo instance
 	e := echo.New()
 
-	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	// Routes
 	e.GET("/", hello)
 	e.GET("/health", func(c echo.Context) error {
 		return c.String(http.StatusOK, "OK")
 	})
 
-	// Start server
-	e.Logger.Fatal(e.Start(":1323"))
+	// 👇 สำคัญมาก
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	e.Logger.Fatal(e.Start(":" + port))
 }
 
-// Handler
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
 }
