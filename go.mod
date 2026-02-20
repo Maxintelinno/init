cbe4cfc6e15e58f66fb44ac0524c7f79312fd994
Osacn thakrit Ratchakote <osacn_thakrit.ratchakote@krungthai.com>
2026-02-20 16:51:20 +0700

    add connect DB

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module sport-hub-be
 
 go 1.25.4
 
-require github.com/labstack/echo/v4 v4.15.0
+require (
+	github.com/labstack/echo/v4 v4.15.0
+	github.com/lib/pq v1.11.2
+)
 
 require (
 	github.com/labstack/gommon v0.4.2 // indirect
--- main.go
+++ main.go
@@ -1,36 +1,50 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	e := echo.New()
-
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/", hello)
-	e.GET("/health", func(c echo.Context) error {
-		return c.String(http.StatusOK, "OK")
-	})
-	e.GET("/*", func(c echo.Context) error {
+	// 🔥 Connect DB
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL not set")
+	}
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("✅ Connected to PostgreSQL")
+
+	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "API Running")
 	})
 
-	// 👇 สำคัญมาก
+	e.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "OK")
+	})
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		panic("PORT not set")
 	}
 
 	e.Logger.Fatal(e.Start("0.0.0.0:" + port))
-}
-
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
 }
